a3b31b93832ef9a3b983be62925a04a0c39db47e
mcmonkeyman <eoin@eoin.ie>
2016-04-12 23:02:30 -0400

    Update madlib

--- madlib.rb
+++ madlib.rb
@@ -52,20 +52,19 @@ Number = gets.chomp
 puts "Plesae type a Adjective"
 Adjective_2 = gets.chomp
 
-
 puts " Christmas Carol Word Fun: Merry Songs & Stories
 The Christmas Song " + Type_Of_Food_Plural +
 " roasting on an open fire. " +
 Celebrity + " nipping at your " + Part_Of_The_Body + ". Yuletide " +
 Plural_Noun + " being sung by a choir And folks  dressed up like " +
 Occupation_Plural + ".Everybody knows a " +
-Noun + " and some mistletoe. Help to make the  " + Plural_Noun_2 + "
-night. Tiny tots with their " + Part_Of_The_Body_Plural_2 + 
+Noun + " and some mistletoe. Help to make the  " + Plural_Noun_2 +
+"night. Tiny tots with their " + Part_Of_The_Body_Plural_2 +
 " all aglow. Will find it hard to  " + Verb + " tonight. They know that " +
 Another_Celebrity_Male + " is on his way. He's loaded lots of  " +
 Plural_Noun3 + " and " + Plural_Noun4 + " on his sleigh.And every  " +
-Occupation + " is gonna try. To see if  " + Animal_Plural + " really know
-how to fly.And so, I'm offering this  "+ Adjective + " phraseFor  " +
+Occupation + " is gonna try. To see if  " + Animal_Plural +
+" really know how to fly.And so, I'm offering this  "+ Adjective + " phraseFor  " +
 Occupation_Plural_2 + " from  " + Number +
 " to ninety-two. Although its been said many times, many ways, " +
 Adjective_2 + " Christmas to you!"
