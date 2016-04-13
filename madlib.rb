69bee40d155aba6a033a2edcd12ef20e73910bfa
mcmonkeyman <eoin@eoin.ie>
2016-04-12 23:01:11 -0400

    Add madlib

---
+++ madlib.rb
@@ -0,0 +1,71 @@
+puts "Plesae type a Type Of Food (Plural)"
+Type_Of_Food_Plural = gets.chomp
+
+puts "Plesae type a Celebrity"
+Celebrity = gets.chomp
+
+puts "Plesae type a Part Of The Body"
+Part_Of_The_Body = gets.chomp
+
+puts "Plesae type a Plural Noun"
+Plural_Noun = gets.chomp
+
+puts "Plesae type a Occupation (Plural)"
+Occupation_Plural = gets.chomp
+
+puts "Plesae type a Noun"
+Noun = gets.chomp
+
+puts "Plesae type a Plural Noun"
+Plural_Noun_2 = gets.chomp
+
+puts "Plesae type a Part Of The Body (Plural)"
+Part_Of_The_Body_Plural_2 = gets.chomp
+
+puts "Plesae type a Verb"
+Verb = gets.chomp
+
+puts "Plesae type a Another Celebrity (Male)"
+Another_Celebrity_Male = gets.chomp
+
+puts "Plesae type a Plural Noun"
+Plural_Noun3 = gets.chomp
+
+puts "Plesae type a Plural Noun"
+Plural_Noun4 = gets.chomp
+
+puts "Plesae type a Occupation"
+Occupation = gets.chomp
+
+puts "Plesae type a Animal (Plural)"
+Animal_Plural = gets.chomp
+
+puts "Plesae type a Adjective"
+Adjective = gets.chomp
+
+puts "Plesae type a Occupation (Plural)"
+Occupation_Plural_2 = gets.chomp
+
+puts "Plesae type a Number"
+Number = gets.chomp
+
+puts "Plesae type a Adjective"
+Adjective_2 = gets.chomp
+
+
+puts " Christmas Carol Word Fun: Merry Songs & Stories
+The Christmas Song " + Type_Of_Food_Plural +
+" roasting on an open fire. " +
+Celebrity + " nipping at your " + Part_Of_The_Body + ". Yuletide " +
+Plural_Noun + " being sung by a choir And folks  dressed up like " +
+Occupation_Plural + ".Everybody knows a " +
+Noun + " and some mistletoe. Help to make the  " + Plural_Noun_2 + "
+night. Tiny tots with their " + Part_Of_The_Body_Plural_2 + 
+" all aglow. Will find it hard to  " + Verb + " tonight. They know that " +
+Another_Celebrity_Male + " is on his way. He's loaded lots of  " +
+Plural_Noun3 + " and " + Plural_Noun4 + " on his sleigh.And every  " +
+Occupation + " is gonna try. To see if  " + Animal_Plural + " really know
+how to fly.And so, I'm offering this  "+ Adjective + " phraseFor  " +
+Occupation_Plural_2 + " from  " + Number +
+" to ninety-two. Although its been said many times, many ways, " +
+Adjective_2 + " Christmas to you!"
